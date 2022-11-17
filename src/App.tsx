37bce4ee186e693fb4c992518058e3d26594c662
wujuno <86146780+wujuno@users.noreply.github.com>
2022-11-17 22:42:32 +0900

    Slider part Two

--- src/App.tsx
+++ src/App.tsx
@@ -26,42 +26,52 @@ box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const box = {
-  invisible: {
-    x: 500,
+  entry: (back:boolean) => ({
+    x: back ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
     x: 0,
     opacity: 1,
     scale: 1,
     transition: {
-      duration: 1,
+      duration: 0.3,
     },
   },
-  exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 1 } },
+  exit: (back:boolean) => ({
+     x: back ? 500 :-500, 
+     opacity: 0, 
+     scale: 0, 
+     transition:{ 
+      duration: 0.3 
+    } })
 };
 
 function App() {
   const [visible, setVisible] = useState(1);
-  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  const [back , setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));}
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));}
   return (
     <Wrapper>
-      <AnimatePresence>
-       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-          i === visible ? (
+      <AnimatePresence exitBeforeEnter custom={back}>
+       
             <Box
+              custom={back}
               variants={box}
-              initial="invisible"
-              animate="visible"
+              initial="entry"
+              animate="center"
               exit="exit"
-              key={i}
+              key={visible}
             >
-              {i}
+              {visible}
             </Box>
-          ) : null
-        )}
+          
       </AnimatePresence>
       <button onClick={nextPlease}>next</button>
       <button onClick={prevPlease}>prev</button>
