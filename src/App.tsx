485cd0b410406f6baae64ccf9d4cbd0bc1ac8f62
wujuno <86146780+wujuno@users.noreply.github.com>
2022-11-17 23:56:18 +0900

    Final Project part Two

--- src/App.tsx
+++ src/App.tsx
@@ -37,24 +37,30 @@ const Overlay = styled(motion.div)`
   align-items: center;
 `;
 
+const overlay = {
+  hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+};
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggle = () => setClicked((prev) => !prev);
+  const [id, setId] = useState<null|string>(null);
   return (
-    <Wrapper onClick={toggle}>
+    <Wrapper>
       <Grid>
-        <Box layoutId="hello"></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
+      {["1", "2", "3", "4"].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
       </Grid>
       <AnimatePresence>
-        {clicked ? <Overlay 
-      initial={{opacity: 0}} 
-      animate={{opacity: 1}} 
-      exit={{opacity: 0}}>
-        <Box layoutId="hello" style={{width: 400, height: 200}}/>
+        {id ? <Overlay 
+        onClick={()=>(setId(null))}
+        variants={overlay}
+      initial="hidden" 
+      animate="visible"
+      exit="exit"
+      >
+        <Box layoutId={id} style={{width: 400, height: 200}}/>
         </Overlay> : null}</AnimatePresence>
     </Wrapper>
   );
